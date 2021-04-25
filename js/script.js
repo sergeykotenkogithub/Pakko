deaf9c30ced606c6b5aaa7a6a27d18f8f8f8ec4f
sergeykotenko <sergeykotenko@bk.ru>
2021-04-25 07:29:02 +0300

    perfect before commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 const iconMenu = document.querySelector('.menu_icon')
 const menuBody = document.querySelector('.header_menu');
-const tarifText = document.querySelector('.tarif__text');
 if(iconMenu){    
     iconMenu.addEventListener("click", function(e) {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');    
         menuBody.classList.toggle('_active');
-        tarifText.classList.toggle('_active');
     });
 }
